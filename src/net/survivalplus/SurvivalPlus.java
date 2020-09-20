45710628650835c0e37071732d596c8b45f357dc
Destitution-SC <43219594+Destitution-SC@users.noreply.github.com>
2020-09-20 23:04:55 +0200

    initial commit

---
+++ src/net/survivalplus/SurvivalPlus.java
@@ -0,0 +1,16 @@
+package net.survivalplus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SurvivalPlus extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
